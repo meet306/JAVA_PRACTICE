0397cbe0125c6ee0a1f9f35833217386bd841305
meet306 <pmeet6042@gmail.com>
2026-08-15 11:06:48 +0530

    feat: add two.java to display personal information to the console

---
+++ Variables_DataTypes/two.java
@@ -0,0 +1,8 @@
+public class two {
+    public static void main(String[] args) {
+        System.out.println("My Name is Meet Patel");
+        System.out.println("My Age is 20");
+        System.out.println("My City is Ahmedabad");
+
+    }
+}
