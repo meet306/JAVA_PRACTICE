e99b051fd818e1586ebaff9c54b2219303962ec7
meet306 <pmeet6042@gmail.com>
2026-08-15 14:33:44 +0530

    feat: add new Java classes for basic arithmetic and operator practice exercises

---
+++ operators/seven.java
@@ -0,0 +1,17 @@
+package operators;
+import java.util.Scanner;
+public class seven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+
+        int s = n*n;
+        int c = n*n*n;
+        System.out.println(s);
+        System.out.println(c);
+
+
+        
+    }
+}
---
+++ operators/ten.java
@@ -0,0 +1,25 @@
+package operators;
+import java.util.Scanner;
+public class ten {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        a++;
+        System.out.println(a);
+        ++a;
+        System.out.println(a);
+        b--;
+        System.out.println(b);
+        --b;
+        System.out.println(b);
+        
+
+        
+    }
+}   
